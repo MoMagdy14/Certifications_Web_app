667c92202297de766095860a52a5718c6f36ae92
MoMagdy14 <14momagdi@gmail.com>
2020-12-21 22:07:49 +0000

    xd

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, send_file
 from flask_session import Session
 from PIL import Image, ImageDraw, ImageFont
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+db = SQL("sqlite:///data.db")
 
 
 @app.route("/")
@@ -25,15 +27,22 @@ def ranking():
 @app.route("/certification", methods= ["GET","POST"])
 def certification():
     if request.method == "POST":
+        course = request.form.get("course")
+        email = request.form.get("email")
+        name = request.form.get("name")
+        value = db.execute("Select * from members WHERE course=:course and email=:email", course=course,email=email)
+        if (True):
+            db.execute("UPDATE members set status = status + 1 WHERE course=:course and email=:email", course=course,email=email)
+            im = Image.open(r'certificate.jpg')
+            d = ImageDraw.Draw(im)
+            location = (180, 270)
+            text_color = (0, 0, 0)
+            font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24)
+            d.text(location, name, fill = text_color,font=font)
+            im.save("certificate_" + "1" + ".pdf")
+            return send_file("certificate_" + "1" + ".pdf", as_attachment=True)
 
-        return render_template("index.html")
+
 
     else:
-        im = Image.open(r'certificate.jpg')
-        d = ImageDraw.Draw(im)
-        location = (360, 298)
-        text_color = (0, 137, 209)
-        # font = ImageFont.truetype("arial.ttf", 120)
-        d.text(location, "text here", fill = text_color)
-        im.save("certificate_" + "1" + ".pdf")
         return render_template("certification.html")
